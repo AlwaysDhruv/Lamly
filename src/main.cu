e3fa7c2c9eb345122e0a25738e089c34d9ed5f2a
AlwaysDhruv <sonavanebharat92@gmail.com>
2026-05-04 15:14:04 +0530

    Linear Projection...Done

--- src/main.cu
+++ src/main.cu
@@ -8,11 +8,17 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	Tokenization tk;
+
 	vector<string> tokens;
 	vector<long long> token_ids;
+
 	//tk.fit("../data/test.txt", 72);
 	tk.encoding("../data/test2.txt", tokens, token_ids);
+
 	Transformer tr(token_ids);
-	tr.ready();
+
+	tr.input_embedding();
+	tr.linear_projection();
+
 	return 0;
 }
