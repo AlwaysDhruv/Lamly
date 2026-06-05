a18bb65d8b8bfde782a107945adba6c93594eedb
AlwaysDhruv <sonavanebharat92@gmail.com>
2026-06-05 14:19:20 +0530

    .

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,61 @@
 #include <vector>
 #include "./include/BPE.hpp"
 #include "./include/Transformer.hpp"
+#include "./include/Display.hpp"
 
 using namespace std;
+
+
+void save_tokens(
+    const vector<long long>& token_ids,
+    const string& path)
+{
+    ofstream file(path, ios::binary);
+
+    size_t size = token_ids.size();
+
+    file.write(
+        reinterpret_cast<const char*>(&size),
+        sizeof(size));
+
+    file.write(
+        reinterpret_cast<const char*>(token_ids.data()),
+        size * sizeof(long long));
+}
+
+vector<long long> load_tokens(
+    const string& path)
+{
+    ifstream file(path, ios::binary);
+
+    size_t size;
+
+    file.read(
+        reinterpret_cast<char*>(&size),
+        sizeof(size));
+
+    vector<long long> token_ids(size);
+
+    file.read(
+        reinterpret_cast<char*>(token_ids.data()),
+        size * sizeof(long long));
+
+    return token_ids;
+}
 
 int main(int argc, char const *argv[])
 {
 	Tokenization tk;
 	
 	vector<string> tokens;
-	vector<long long> token_ids;
+	vector<long long> token_ids = load_tokens("tokens.bin");
 
-	//tk.fit("../data/test.txt", 1000);
-	cout << "Encoding starts for test.txt....";
+	// //tk.fit("../data/test.txt", 1000);
+	// cout << "Encoding starts for test.txt....";
 	
-	tk.encoding("../data/test.txt", tokens, token_ids);
-	
-	cout << "done with " << token_ids.size() << " ids....." << endl;
+	// tk.encoding("../data/test.txt", tokens, token_ids);
+	// save_tokens(token_ids, "tokens.bin");
+	// cout << "done with " << token_ids.size() << " ids....." << endl;
 
 	Transformer tr(token_ids);
 
